30da804797a7208751946b9fc3acafe9848c3124
THEKAN01 <146573817+THEKAN01@users.noreply.github.com>
2026-02-07 22:46:00 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Utilise './' si tu déploies dans un sous-dossier, sinon '/'
+  server: {
+    port: 3000, // Optionnel : force le port local à 3000
+  },
+  build: {
+    outDir: 'dist', // Le dossier qui sera généré pour le déploiement
+  }
+})s
